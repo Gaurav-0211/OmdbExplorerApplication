60c8ddfdb21d0d0a51833e483721d9bc484454ba
Gaurav Kumar <gauravkr0211@gmail.com>
2025-11-26 16:03:19 +0530

    _Fix: Web Config

--- src/main/java/com/omdb/config/WebConfig.java
+++ src/main/java/com/omdb/config/WebConfig.java
@@ -6,23 +6,18 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer{
+public class WebConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
-
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowCredentials(true)
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
                         .allowedHeaders("*");
             }
-
         };
     }
 }
-
-
